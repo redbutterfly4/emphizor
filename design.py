1663345446ad70b8b7bd1a5d2505ffcdc97efde2
Radamir <dermeister20032132134@gmail.com>
2025-07-01 19:27:35 +0300

    "Add tag" button create an empty dialog now

--- design.py
+++ design.py
@@ -15,7 +15,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QListView,
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
     QStatusBar, QWidget)
 
 class Ui_MainWindow(object):
@@ -25,6 +26,17 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.listView = QListView(self.centralwidget)
+        self.listView.setObjectName(u"listView")
+        self.listView.setGeometry(QRect(10, 0, 111, 561))
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 10, 66, 18))
+        self.label.setFrameShape(QFrame.Shape.NoFrame)
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(10, 30, 94, 26))
+        self.pushButton.setAutoDefault(False)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
@@ -36,10 +48,15 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
+        self.pushButton.setDefault(True)
+
+
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Emphizer", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Your tags:", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Add tag ", None))
     # retranslateUi
 
--- gui.py
+++ gui.py
@@ -1,5 +1,26 @@
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication, QMainWindow, QDialog
+from design import Ui_MainWindow
+class MainWindow(QMainWindow):
+    tags : list[str]
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.add_tag_clicked)
 
-app = QApplication()
+    def add_tag_clicked(self):
+        dialog = QDialog(self)
+        dialog.setFixedHeight(400)
+        dialog.setFixedWidth(400)
+        dialog.setWindowTitle('Add tag')
+        dialog.show()
+        
 
-app.exec()
+def main():
+    app = QApplication()
+    window = MainWindow()
+    window.show()
+    app.exec()
+
+if __name__ == "__main__":
+    main()
