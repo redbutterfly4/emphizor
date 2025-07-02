f44b505d59787e688e866a6071de01bdcc77155a
Radamir <dermeister20032132134@gmail.com>
2025-07-02 09:18:18 +0300

    extended tag window, can enter text and close dialog

--- gui.py
+++ gui.py
@@ -1,26 +1,44 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QLineEdit, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QLineEdit, QVBoxLayout, QLabel, QHBoxLayout, QDialogButtonBox
 from design import Ui_MainWindow
 class MainWindow(QMainWindow):
     tags : list[str]
+    tag_len_limit = 50
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.add_tag_clicked)
 
-    def add_tag_clicked(self):
+    def enter_string_dialog(self, label_message, title):
         temp_dialog = QDialog(self)
         temp_dialog.setFixedHeight(400)
         temp_dialog.setFixedWidth(400)
-        temp_dialog.setWindowTitle('Add tag')
-        temp_layout = QVBoxLayout(temp_dialog)
+        temp_dialog.setWindowTitle(title)
+        temp_layout = QHBoxLayout(temp_dialog)
         temp_label = QLabel(temp_dialog)
-        temp_label.setFixedWidth(150)
-        temp_label.setText('Enter tag name:')
+        temp_label.setFixedWidth(75)
+        temp_label.setText(label_message)
+        temp_line_edit = QLineEdit(temp_dialog)
+        temp_line_edit.setFixedWidth(250)
+        temp_layout.addWidget(temp_label)
+        temp_layout.addWidget(temp_line_edit)
 
-        
+        temp_button_box = QDialogButtonBox(QDialogButtonBox.Ok)
+
+        temp_button_box.setParent(temp_dialog)
+        temp_layout.addWidget(temp_button_box)
+
+        def accept_button_pressed():
+            temp_dialog.close()
+            
         
         temp_dialog.show()
+        temp_button_box.accepted.connect(accept_button_pressed)
+        return temp_line_edit.text()
+
+
+    def add_tag_clicked(self):
+       self.enter_string_dialog('Enter tag: ', 'Add tag')
 
         
         
